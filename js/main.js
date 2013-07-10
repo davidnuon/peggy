f9687c461a8403bd12f10ff05d00b11d95150e94
David Nuon <david@davidnuon.com>
2013-07-10 16:09:10 -0700

    Fixed loading order

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
 (function () {
+	var codebox, venster, closebox, pane, editor, demo_code;
+
+	venster = document.getElementById('venster');
+	codebox = document.getElementById('codebox');
+	closebox = document.getElementById('close');
+	pane = document.getElementById('modal-editor');
+	demo_code = document.getElementById('demo_code');
+
+	venster.contentDocument.documentElement.style.background = '#fff';
+
+	var set_frame = function(str) {
+		venster.contentDocument.documentElement.getElementsByTagName('body')[0].innerHTML = str;
+	}
+
+	var options = CodeMirror.defaults;
+	options.lineNumbers = true;
+	options.value = demo_code.innerHTML;
+
 
 	window.onload = function () {
 
-		var codebox, venster, closebox, editor, demo_code;
-
-		venster = document.getElementById('venster');
-		codebox = document.getElementById('codebox');
-		closebox = document.getElementById('close');
-		editor = document.getElementById('modal-editor');
-		demo_code = document.getElementById('demo_code');
-
-
-		venster.contentDocument.documentElement.style.background = '#fff';
-
-
-		var the_code = function() { return codebox.value; };
-		var set_frame = function(str) {
-			venster.contentDocument.documentElement.getElementsByTagName('body')[0].innerHTML = str;
-		}
-
-
-		var options = CodeMirror.defaults;
-		options.lineNumbers = true;
-		options.value = demo_code.innerHTML;
-
-
-
-		var editor = CodeMirror(codebox, options);
+		editor = CodeMirror(codebox, options);
 		editor.on("change", function () {
 			set_frame(editor.getValue());
 		});
 
+		window.onresize();
 
 		set_frame(editor.getValue())
-
 	};
+
+	window.onresize = function () {
+		editor.setSize("100%", pane.offsetHeight - 40);
+	}
 })();
