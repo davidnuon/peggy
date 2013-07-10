2a0469740bd7fb84fe20a933df809a0d54237107
David Nuon <david@davidnuon.com>
2013-07-10 16:19:45 -0700

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
 (function () {
-	var codebox, venster, closebox, pane, editor, demo_code;
-
+	var codebox, venster, pane, editor, demo_code;
+	
+	// This is the iframe where we will be updating content
 	venster = document.getElementById('venster');
-	codebox = document.getElementById('codebox');
-	closebox = document.getElementById('close');
+
+	// Left-side pane where we have controls
 	pane = document.getElementById('modal-editor');
+	
+	// <div> to hold the codemirror editor
+	codebox = document.getElementById('codebox');
+
+	// <template> that holds initial content for the editor
 	demo_code = document.getElementById('demo_code');
 
+	// Set the iframe background color to white
 	venster.contentDocument.documentElement.style.background = '#fff';
 
+	// Helper to set the content of the document of the iframe
 	var set_frame = function(str) {
 		venster.contentDocument.documentElement.getElementsByTagName('body')[0].innerHTML = str;
 	}
 
+
+	// Codemirror options
 	var options = CodeMirror.defaults;
 	options.lineNumbers = true;
 	options.value = demo_code.innerHTML;
-
 
 	window.onload = function () {
 
