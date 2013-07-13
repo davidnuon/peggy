0210e3c9dfe716457ea47c59188dee50f3ef4307
David Nuon <david@davidnuon.com>
2013-07-13 14:37:42 -0700

    Added Mock App to webpy

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from jinja2 import Environment, PackageLoader
 env = Environment(loader=PackageLoader('app', 'templates'))
 
 urls = (
+    '/app/(.*)', 'App',
+    '/app', 'App',
+    
     '/(.*)', 'Index'
 )
 app = web.application(urls, globals())
@@ -14,6 +17,22 @@ class Index:
     	template = env.get_template('index.html')
 
     	return template.render(css=[{"url":"/static/css/landing.css"}]) 
+
+
+class App:        
+    def GET(self):
+    	template = env.get_template('app.html')
+
+        return template.render(css=[
+            {"url":"/static/css/style.css"},
+            {"url":"/static/codemirror/lib/codemirror.css"},
+        ], js=[
+            {"url":"/static/js/jquery.js"},
+            {"url":"/static/codemirror/lib/codemirror.js"},
+            {"url":"/static/js/doc_store.js"},
+            {"url":"/static/js/main.js"},
+        ])
+
 
 if __name__ == "__main__":
     app.run()
