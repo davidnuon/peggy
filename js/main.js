2579e52dded9cabf2fd22308012fd781142c7d0f
David Nuon <david@davidnuon.com>
2013-07-12 20:59:36 -0700

    Moving things on to jquery

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 (function () {
-	// Helpers
-	function $(id) {
-		return document.getElementById(id);
-	}
-
-	function getChildNodes(el) {
-		var out = [];
- 
-		for (var i = el.childNodes.length - 1; i >= 0; i--) {
-			out[out.length] = el.childNodes[i];
-		};
- 
- 		return out;
-	}
-
-
 	// Helper to set the content of the document of the iframe
 	var set_frame = function(str) {
 		venster.contentDocument.documentElement.getElementsByTagName('body')[0].innerHTML = str;
@@ -25,20 +9,20 @@
 		venster.contentDocument.documentElement.getElementsByTagName('style')[0].innerHTML = str;
 	}
 
-	var codebox, venster, pane, editor, demo_code;
+	var $codebox, venster, $pane, $editor, $demo_code;
 	var the_docs = document_store();
 
 	// This is the iframe where we will be updating content
-	venster = $('venster');
+	venster = document.getElementById('venster');
 
 	// Left-side pane where we have controls
-	pane = $('modal-editor');
+	$pane = $('#modal-editor');
 	
 	// <div> to hold the codemirror editor
-	codebox = $('codebox');
+	$codebox = $('#codebox');
 
 	// <template> that holds initial content for the editor
-	demo_code = $('demo_code');
+	$demo_code = $('#demo_code');
 
 	// Making the document
 	// Set the iframe background color to white
@@ -49,16 +33,15 @@
 
 
 	// Editor Controls
-	var the_tabs = getChildNodes($('tabs'));
-	for (var i = the_tabs.length - 1; i >= 0; i--) {
-		if(the_tabs[i].getAttribute) {
-			var id = the_tabs[i].getAttribute('id');
+	var $the_tabs = $('#tabs li');
+	$the_tabs.each(
+		function(n, e) {
+			var id = $(e).attr('id');
 			the_docs.add(id);
-		};
-	};
+		}
+	);
 
-
-	the_docs.docs.css.set('body {\n\tbackground:#fff;\n};');
+	the_docs.docs.css.set('body {\n\tbackground:#fff;\n}');
 
 	the_docs.change('html');
 	the_docs.set(demo_code.innerHTML);
@@ -70,31 +53,18 @@
 	options.lineWrapping = true;
 
 	// Make all tabs clickable
-	the_tabs.map( function(e) {
-		e.onclick = function(event) {
-			var me = this;
-			var siblings =  getChildNodes(me.parentNode);
-			
-			if(me.getAttribute('class') === 'selected') { return; }
+	$the_tabs.each( function(n, e) {
+		e.onclick = function(event) {			
+			$("#tabs li").removeClass("selected");
+			$(this).addClass("selected");
 
-			for (var i = siblings.length - 1; i >= 0; i--) {
-				if(siblings[i].setAttribute) {				
-					if(siblings[i] === this) {
-						siblings[i].setAttribute('class', 'selected');
-					} else {
-						siblings[i].setAttribute('class', '');
-					} 
-				}
-			};
-			
-
-			the_docs.change(me.getAttribute('id'));
+			the_docs.change($(this).attr('id'));
 			editor.setValue(the_docs.get());
 		};
 	});
 
-	window.onload = function () {
-		$('html').setAttribute('class', 'selected');
+	$(function () {
+		$('#html').attr('class', 'selected');
 
 		editor = CodeMirror(codebox, options);
 		editor.on("change", function () {
@@ -114,10 +84,10 @@
 
 		set_css(the_docs.docs.css.get());
 		set_frame(the_docs.docs.html.get());
-	};
+	});
 
 	window.onresize = function () {
-		editor.setSize("100%", pane.offsetHeight - 40);
+		editor.setSize("100%", $pane.height() - 40);
 	}
 
 })();
